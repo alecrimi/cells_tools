d94ee8461f29608cd29de187eb0c22cf02c9dbec
Alessandro Crimi <alessandro.crimi@iit.it>
2018-07-13 13:27:14 +0200

    Create mean_int_plate.m

---
+++ mean_int_plate.m
@@ -0,0 +1,29 @@
+clear all;
+
+strImPath='I:\KristinaAirich_384Well_Screen_Plate_4_0207182044_1';
+
+% get all the tif file in the folder
+cellFiles=dir(strImPath);
+cellFiles={cellFiles.name}';
+cellFiles(cellfun('isempty', strfind(cellFiles, 'tif')))=[];
+
+ 
+matOutput=nan(numel(cellFiles)/2, 1);  %This is the variable with the file names
+outer_count = 1;
+mean_int_values= zeros(numel(cellFiles));  %This is the variable with the intensities
+
+for i = 1 : 2 : numel(cellFiles)
+    i
+      
+        blue_channel =  imread(fullfile(strImPath, cellFiles{i}), 'tif');
+        green_channel = imread(fullfile(strImPath, cellFiles{i+1}), 'tif');
+    
+        mean_int_b = mean(mean(blue_channel))
+        mean_int_g = mean(mean(green_channel))
+        
+        mean_int_values(i) = mean_int_b;
+        mean_int_values(i+1) = mean_int_g;
+        
+end
+
+ 
