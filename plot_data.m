6fbff9a535d5f3170feb1c7e0a2e36a378989535
Alessandro Crimi <alessandro.crimi@iit.it>
2018-07-23 16:54:00 +0200

    Create plot_data.m

---
+++ plot_data.m
@@ -0,0 +1,30 @@
+function plot_data(filename)
+ 
+data = csvread(filename);
+
+% Average per well
+data_averaged = zeros(length(data)/2,5);
+result_count = 1;
+for jj = 1 : 2:  length(data)
+    data_averaged(result_count,:)  = mean( data(jj:jj+1,:)  );
+    result_count = result_count +1;
+end
+
+% The convetion is to have 
+% in the first column Cell count
+% in the second column Norm Cell Count
+% in the thid column Mean intensity 
+% in the fourth column Area
+% in the fifth column Green Cell count
+
+cell_count = data_averaged(:,1);
+n_cell_count = data_averaged(:,2);
+mean_int = data_averaged(:,3);
+area = data_averaged(:,4);
+GFP_cell_count = data_averaged(:,5);
+
+plot_heatmap(cell_count, 'cell_count');
+plot_heatmap(n_cell_count, 'n_cell_count');
+plot_heatmap(mean_int, 'mean_int');
+plot_heatmap(area, 'area');
+plot_heatmap(GFP_cell_count, 'GFP_cell_count');
