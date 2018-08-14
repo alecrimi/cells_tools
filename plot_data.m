4c8f2da8544fec849885912d1c7650021b8fa203
Alessandro Crimi <alessandro.crimi@iit.it>
2018-08-14 17:23:26 +0200

    Update plot_data.m

--- plot_data.m
+++ plot_data.m
@@ -2,6 +2,7 @@ function plot_data(filename)
  
 data = csvread(filename);
 
+%{
 % Average per well
 data_averaged = zeros(length(data)/2,5);
 result_count = 1;
@@ -9,6 +10,7 @@ for jj = 1 : 2:  length(data)
     data_averaged(result_count,:)  = mean( data(jj:jj+1,:)  );
     result_count = result_count +1;
 end
+%}
 
 % The convetion is to have 
 % in the first column Cell count
