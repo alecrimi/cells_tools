e795f596a4eee0ba20b755e452bad7a456a06318
Alessandro Crimi <alessandro.crimi@iit.it>
2018-08-22 15:00:38 +0200

    Update run_all_RG.m
    Automatic geneID matching and finding of control/case filles

--- run_all_RG.m
+++ run_all_RG.m
@@ -1,5 +1,4 @@
 % RG_all
-
 list = dir('*.tif');
 
 results = zeros(length(list)/2,5);
@@ -40,23 +39,94 @@ for array = char(double('A'):double('P')) % create character array from 'A' to '
     end
     count_letters = count_letters +24;
 end 
- 
 results_averaged = num2cell(results_averaged);
-tot_feat = [ results_averaged plate_list];
 
-% Save the average features
+% Match sampleID&GeneSymbol 
+% Find file control
+dinfo = dir('.');
+pattern = 'Samples'; 
+false_match = cellfun(@isempty, regexp({dinfo.name}, pattern) );
+dinfo(false_match) = [];   %delete entries that accidentally matched
+filename_gene = dinfo.name;
+%filename_gene = 'Samples Dest_P_65-72.csv';
+fid = fopen(filename_gene, 'rt');  
+genelist = textscan(fid,'%s%s%s%s%s%s','HeaderLines',1,'Delimiter',',','EndOfLine','\r\n','ReturnOnError',false);
+fclose(fid);
+dest_well = genelist{2};
+dest_well = dest_well(1:2:end);
+sampleID = genelist{5};
+sampleID = sampleID(1:2:end);
+geneSymbol  = genelist{6};
+geneSymbol  = geneSymbol(1:2:end);
+matched_sampleID = strings(size(plate_list));
+matched_gene = strings(size(plate_list));
+for kk = 1 : length(plate_list)
+    
+    for jj = 1 : length(sampleID)
+        tf = strcmp(plate_list(kk),string(dest_well(jj))); 
+
+        if(tf) 
+           matched_sampleID(kk) = sampleID(jj);
+           matched_gene(kk) = geneSymbol(jj);
+        end
+    end
+end
+
+% Match sourcewell
+% Find file control
+dinfo = dir('.');
+pattern = 'controls'; 
+false_match = cellfun(@isempty, regexp({dinfo.name}, pattern) );
+dinfo(false_match) = [];   %delete entries that accidentally matched
+filename_control = dinfo.name;
+%filename_gene = 'Picking List controls DD 65 - 72.csv';
+fid = fopen(filename_control, 'rt');  
+sourcelist = textscan(fid,'%s%s%s%s','HeaderLines',1,'Delimiter',',','EndOfLine','\r\n','ReturnOnError',false);
+fclose(fid);
+dest_well = sourcelist{2};
+dest_well = dest_well(1:2:end);
+source_well= sourcelist{4};
+source_well = source_well(1:2:end); 
+
+matched_source = strings(size(plate_list)); 
+for kk = 1 : length(plate_list)
+    
+    for jj = 1 : length(dest_well)
+        tf = strcmp(plate_list(kk),string(dest_well(jj))); 
+
+        if(tf) 
+           matched_source(kk) = source_well(jj); 
+        end
+    end
+end
+
+%Save everything
+tot_feat = [ results_averaged plate_list matched_sampleID matched_gene matched_source];
 %csvwrite('avg_results_features.csv',results_averaged);
 fid = fopen('avg_results_features.csv','wt');
 if fid>0
-     fprintf(fid,'CellCount,NormCellCount,MeanInt,Area,GreenCellCount,Well\n');
+     fprintf(fid,'CellCount,NormCellCount,MeanInt,Area,GreenCellCount,Well,Sampleid,GeneSymbol,SourceWell\n');
      for k=1:size(tot_feat,1)
-         fprintf(fid,'%s,%s,%s,%s,%s,%s\n',tot_feat{k,:}); %%
+         fprintf(fid,'%s,%s,%s,%s,%s,%s,%s,%s,%s\n',tot_feat{k,:}); %%
      end
      fclose(fid);
 end
 
 % Plot and save averaged heatmaps
 plot_data('avg_results_features.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+%Rubbish to be removed
 
 %{
 % Compute data per well
